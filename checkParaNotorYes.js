b61d066e5df32e7bfd1dc31b211a788b6f18213f
Sarovar Shaikh <sarwarshaikh3622@gmail.com>
2022-07-14 23:18:21 +0530

    add Objecrs

---
+++ checkParaNotorYes.js
@@ -0,0 +1,9 @@
+// Complete the function in the editor. In which you are given two objects as a parameter.In which you have to merge two objects and return a single object.
+function Check(obj1) {
+    if((Object.entries(obj1)).length === 0){
+      return 'false'
+    } else{
+      return ('true')
+      
+    }
+ }
---
+++ sumObjectMembers.js
@@ -0,0 +1,4 @@
+// You are given a function Check which takes an object Obj as a parameter.Your taks is to find out the sum of the three data members of the object. The data members are named as p1, p2, p3.
+function Check(obj1) {
+    return obj1.p1+obj1.p2+obj1.p3;
+}
